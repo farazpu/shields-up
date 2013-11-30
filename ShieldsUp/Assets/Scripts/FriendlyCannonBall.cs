19d199c839b1927327aaf8e5a5bb478ccad175ad
farazpu@gmail.com <farazpu@gmail.com@3a825c4b-4d12-e085-09c4-b4dc1f826896>
2013-11-30 18:54:47 +0000

    initial import

---
+++ ShieldsUp/Assets/Scripts/FriendlyCannonBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyCannonBall : MonoBehaviour {
+	public ColorEnum color; 
+
+	public void OnTriggerEnter2D (Collider2D coll) {
+		HandleCollision (coll.gameObject);
+	}
+	
+	private void HandleCollision(GameObject go) {
+		if (go.tag == "EnemyCannonBalls") {
+
+			if(go.GetComponent<EnemyCannonBall>().color == color) {
+				go.transform.parent.gameObject.GetComponent<Gun>().ResetCannonBall(go);
+				Score.score += 2;
+			} else {
+				Score.score -= 2;
+			}
+			transform.parent.gameObject.GetComponent<Gun>().ResetCannonBall(gameObject);
+		}
+
+		if (go.tag == "EnemyCennons") {
+			
+			go.GetComponent<EnemyCannon>().TakeDemage();
+			transform.parent.gameObject.GetComponent<Gun>().ResetCannonBall(gameObject);
+		}
+
+	}
+	
+	public void OnCollisionEnter2D(Collision2D coll) {
+		HandleCollision (coll.gameObject);
+	}
+}
---
+++ ShieldsUp/Assets/Scripts/Shields.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shields : MonoBehaviour {
+
+	public GameObject[] shields;
+	
+	public ColorEnum startShield;
+	
+	// Use this for initialization
+	void Start () {
+		SetActiveShiled (startShield);
+	}
+	
+	public void SetActiveShiled(ColorEnum activeShield) {
+		for (int i = 0; i < shields.Length; i++) {
+			if(i == (int)activeShield)
+				shields[i].SetActive (true);
+			else
+				shields[i].SetActive (false);
+
+		}
+	}
+	
+	public ColorEnum GetSelectedShield() {
+		for (int i = 0; i < shields.Length; i++) {
+			if(shields[i].activeSelf)
+				return (ColorEnum)i;
+		}
+		return ColorEnum.purple;
+	}
+	
+	public void RotateShield() {
+		int nextSelected = ((int)GetSelectedShield ()) + 1;
+		
+		if (nextSelected >= shields.Length)
+			nextSelected = 0;
+		
+		SetActiveShiled ((ColorEnum)nextSelected);
+	}
+	
+	public void OnMouseUp () {
+		RotateShield ();
+	}
+
+	public void Break() {
+		gameObject.GetComponent<Animator> ().enabled = true;
+		Score.score -= 30;
+
+	}
+
+}
