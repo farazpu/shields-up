84ae3b84dd876ebfed99d4423a9bd34acf919688
farazpu@gmail.com <farazpu@gmail.com@3a825c4b-4d12-e085-09c4-b4dc1f826896>
2013-12-01 22:13:51 +0000

    Added welcome screen and small tutorial

--- trunk/ShieldsUp/Assets/Scripts/PausePlay.cs
+++ trunk/ShieldsUp/Assets/Scripts/PausePlay.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class PausePlay : MonoBehaviour {
-
+	public Messages messages;
 	public void OnMouseUp() {
 		
 		if (Time.timeScale == 0) {
 			Time.timeScale = 1;
+			messages.HideAllSplashes();
 		} else {
 			Time.timeScale = 0;
+			messages.ShowPaused();
 		}
 	}
 }
---
+++ trunk/ShieldsUp/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+	public EnemyCannon enemyCannon;
+	public Gun enemyGun;
+	// Use this for initialization
+	void Start () {
+		enemyCannon.transform.Rotate (new Vector3 (0, 0, 55.4236f));
+		int fireForce = enemyGun.fireForce;
+		enemyGun.fireForce = 300;
+		enemyGun.Fire (ColorEnum.red);
+		enemyGun.Fire (ColorEnum.green);
+		enemyGun.Fire (ColorEnum.purple);
+		enemyGun.fireForce = fireForce;
+		enemyCannon.transform.Rotate (new Vector3 (0, 0, -55.4236f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
